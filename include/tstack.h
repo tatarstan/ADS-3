6d0770ade5afbcb54d66adce1fc0e07fa43faae7
anton <ashtanyuk@gmail.com>
2020-04-17 11:42:57 +0300

    Initial commit

---
+++ include/tstack.h
@@ -0,0 +1,28 @@
+const int size=100;
+
+template<typename type>
+class TStack
+{
+   private:
+     type arr[size];
+     int top;
+   public:
+     TStack():top(-1) { }
+     type get() const { 
+        return arr[top];
+     }
+     bool isEmpty() const {
+        return top==-1;
+     }
+     bool isFull() const {
+        return top==size-1;
+     }
+     void pop() {
+        if(top>=0)
+          top--;
+     }
+     void push(type item) {
+       if(top<size-1)
+         arr[++top]=item;
+     }
+};
---
+++ src/alg.cpp
@@ -0,0 +1,12 @@
+#include "tstack.h"
+#include <string>
+
+std::string infx2pstfx(std::string inf)
+{
+
+}
+
+int eval(std::string pst)
+{
+
+}
